5acaf70fc49109d7359b95f69842f4c98727e96a
SirJames115 <81270423+SirJames115@users.noreply.github.com>
2023-11-28 13:24:41 +0100

    First commit: with all pages set up and app functionality halfway implemented

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ const dateElement = document.getElementById("date");
 const clear = document.querySelector(".clear");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+const submit = document.querySelector(".submit");
+
+// Classes
+const CHECK = 'fa-check-circle'
+const UNCHECK = 'fa-circle-thin'
+const LINE_THROUGH = 'LineThough'
 
 // Show the current date
 const options = {
@@ -14,14 +20,54 @@ dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 // Add trip item to the list
 function addTripItem(item, id, done, trash) {
-  const position = "beforeend";
+  if (trash) {
+    return;
+  }
+
+  const DONE = done ? CHECK : UNCHECK
+  const LINE = done ? LINE_THROUGH : ""
+
   const note = `
-    <li class="item">
-                    <i class="fa fa-circle-thin co" job="complete" id="${id}"></i>
-                    <p class="text">${item}</p>
-                    <i class="fa fa-trash-o de" job="delete" id="${id}></i>
-                </li>
-    `;
-    list.insertAdjacentHTML(position, note)
+  <li class="item">
+  <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+  <p class="text ${DONE}">${item}</p>
+  <i class="fa fa-trash-o de" job="delete" id="${id}></i>
+  </li>
+  `;
+  const position = "beforeend";
+  list.insertAdjacentHTML(position, note);
 }
-addTripItem('PC')
+
+let = LIST = [], id = 0;
+
+submit.addEventListener("click", (e) => {
+  const item = input.value;
+  if (item) {
+    addTripItem(item, id, false, false);
+    
+    LIST.push({
+      name:item, id:id, done: false, trash:false
+    })
+  } else {
+    alert(`You cant submit an empty field`);
+  }
+  id++
+  input.value = "";
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.keyCode == 13) {
+    const item = input.value;
+
+    if(item){
+      addTripItem(item, id, false, false);
+
+      LIST.push({
+        name:item, id:id, done: false, trash:false
+      })
+    }
+
+    id++;
+    input.value = "";
+  }
+});
